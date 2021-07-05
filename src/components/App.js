5aee455504357686f3858f2fe15c7a9d5a267365
PIYUSH CHOUDHARY <71705154+Developer-piyush@users.noreply.github.com>
2021-07-06 00:39:53 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,9 @@ import Main from './Main'
 import Web3 from 'web3';
 import './App.css';
 
+//Reload page on network change
+window.ethereum.on('chainChanged', (_chainId) => window.location.reload());
+
 //Declare IPFS
 const ipfsClient = require('ipfs-http-client')
 const ipfs = ipfsClient({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' }) // leaving out the arguments will default to these values
